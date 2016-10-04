bbbd8b615053bf3797d0906d8090163be652f251
Victor Benavente <vbenavente@hotmail.com>
2016-10-04 10:21:00 -0700

    test for helper funcion get_word is passing

--- src/autocomplete/test_autocomplete.py
+++ src/autocomplete/test_autocomplete.py
@@ -1,9 +1,21 @@
 import autocomplete
 
 
-def test_autocomplete():
-    """Tests that a list of options is returned."""
+def test_get_word_token():
+    """Tests that a word is returned as the token."""
     vocabulary = ['fix', 'fax', 'fit', 'fist', 'full', 'finch', 'final', 'finial']
-    complete_me = autocomplete.AutoCompleter(vocabulary, max_completions=4)
-    suggested_words = complete_me('fi')
-    assert isinstance(suggested_words, list)
+    start = autocomplete.AutoCompleter(vocabulary, max_completions=5)
+    start = start.vocabulary.root
+    token = ''
+    start, token = autocomplete.get_word(start, token)
+    assert isinstance(token, str)
+
+
+def test_get_word_start():
+    """Tests that an ordered dictionary is returned as start."""
+    vocabulary = ['fix', 'fax', 'fit', 'fist', 'full', 'finch', 'final', 'finial']
+    start = autocomplete.AutoCompleter(vocabulary, max_completions=5)
+    start = start.vocabulary.root
+    token = ''
+    start, token = autocomplete.get_word(start, token)
+    assert isinstance(start, dict)
